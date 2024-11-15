0230044950fd60d49a5ab5e699f9e2a8811e98cb
enzochou <enzo@hej.ai>
2024-11-15 20:31:01 +0000

    changed prop menu modal accordingly

--- components/MenuEditor.tsx
+++ components/MenuEditor.tsx
@@ -2,21 +2,21 @@ import { View, Text, Pressable } from 'react-native'
 import React, { useContext, useState } from 'react'
 import { MenuContext } from '@/components/contexts/MenuManager';
 import { MenuEdit } from '@/components/modals/MenuEdit';
-import { PropModal } from '@/components/modals/PropModal';
+import { PropMenuModal } from '@/components/modals/PropMenuModal';
 
 export const MenuEditor = () => {
     const menuManager = useContext(MenuContext);
-    const [modalVisible, setModalVisible] = useState(false);
+    const [menuModalVisible, setMenuModalVisible] = useState(false);
     return (
         <View className='flex-auto'>
             <Pressable
-                onPress={() => setModalVisible(true)}
+                onPress={() => setMenuModalVisible(true)}
             >
                 {/* {({ pressed }) => (
                     <Text className='text-2xl text-center font-Norsebold'>{pressed ? 'Editing' : 'Edit'}</Text>
                 )} */}
                     <Text className='text-2xl text-center font-Norsebold'>Add Category</Text>
-                    <PropModal open={modalVisible} onClose={setModalVisible} model='category'/>
+                    <PropMenuModal open={menuModalVisible} onClose={setMenuModalVisible} model='category'/>
             </Pressable>
         </View>
     )
