c57d0804d126d978657a6f65cfa64e707daa14fe
enzochou <enzo@hej.ai>
2024-12-10 16:50:17 +0000

    (WIK) added custom hook to centralize order calls

---
+++ hooks/useOrders.ts
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+import React from 'react';
+
+export const useRetrieveOrders = async(refreshing: boolean, setRefreshing: React.Dispatch<React.SetStateAction<boolean>>) => {
+    // 
+    // const [checkboxes, setCheckBoxes] = useState<Checkbox[]>([]);
+   
+    useEffect(() => {
+        
+    }, []);
+    return;
+};
