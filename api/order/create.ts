8a789f85bc91e08276fe6f724020d159419f1b7a
enzochou <enzo@hej.ai>
2024-12-14 15:45:35 +0000

    added template for api order call

--- api/order/create.ts
+++ api/order/create.ts
@@ -1,3 +1,19 @@
-export default async function (order: order) {
-    return true;
+export default async function (orders: order[]) {
+    let error: unknown | null = null;
+    let message: string = '';
+    try {
+        await new Promise((resolve) =>
+            setTimeout(() =>
+                resolve(1), 1000));
+    } catch (err: unknown) {
+        orders = [];
+        error = true;
+        error = err;
+    } finally {
+        return {
+            orders,
+            error,
+            message
+        };
+    }
 };
